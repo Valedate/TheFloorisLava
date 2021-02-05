8460842966e37c8a913522be248ab4cf7a314af0
Valedate <58189910+Valedate@users.noreply.github.com>
2021-02-05 13:43:33 +0100

    Myroom
    Myroom -  first level

---
+++ Assets/floortrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floortrigger : MonoBehaviour
+{
+    public GameObject telepport;
+    public GameObject player;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = telepport.transform.position;
+        }
+
+    }
+}
---
+++ Assets/hold.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hold : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material red;
+    public Material blue;
+    public GameObject platform;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag =="Player") {
+            platform.transform.GetComponent<Renderer>().material = blue;
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player") {
+            platform.transform.GetComponent<Renderer>().material = red;
+        }
+
+    }
+
+}
